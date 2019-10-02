cfb8b12352f2e6b82edfc854bf8f013562ccdf42
Basil <basilmosa99@gmail.com>
2019-10-02 20:26:53 +0300

    please work

--- Java01.js
+++
@@ -1,9 +0,0 @@
-var property=prompt("Have you ever driven a Mercedes car");
-if(property == 'yes'){
-    alert("You had a nice experiment");
-}else if(property=='no'){
-    alert("I advice you to buy one");
-}
-else {
-alert("I can't help you");
-}
