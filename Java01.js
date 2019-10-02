b85daa54dd1b9beb5b6bda51d003dc1a31e6aa45
Basil <basilmosa99@gmail.com>
2019-10-02 19:30:15 +0300

    last

---
+++ Java01.js
@@ -0,0 +1,9 @@
+var property=prompt("Have you ever driven a Mercedes car");
+if(property == 'yes'){
+    alert("You had a nice experiment");
+}else if(property=='no'){
+    alert("I advice you to buy one");
+}
+else {
+alert("I can't help you");
+}
